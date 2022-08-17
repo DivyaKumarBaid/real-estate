6de8228d066050b78271e26415415f754599d9c8
DivyaKumarBaid <divyakumarbaid.dkb.2001@gmail.com>
2022-08-17 14:32:31 +0530

    Arranged cards

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import Card from './components/Card/Card'
 import './App.css'
+import Data from './data/houseData.json'
 
 export default function App() {
+
+  const [loc, setLoc] = React.useState(null);
+  const [lowerRange, setLow] = React.useState(null);
+  const [UpperRange, setHigh] = React.useState(null);
+  const [houseType, setType] = React.useState(null);
+  const [movein, setMove] = React.useState(null);
+
+  const [popular, setPop] = React.useState(Data.filter(data => data.popularity))
+  const [normalData, setData] = React.useState(Data.filter(data => !data.popularity))
+
+  // const newData = () => { 
+
+  // }
+
   return (
-    <div className="App">
-      <div><Card /></div>
-      <div><Card /></div>
-      <div><Card /></div>
+    <div className="container">
+      <div className="filter"></div>
+      <div className="searchResult">
+        {popular.map((data => <Card obj = {data}/>))}
+        {normalData.map((data => <Card obj = {data}/>))}
+      </div>
     </div>
   )
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,33 +1,56 @@
 import React from 'react'
 import './card.css'
+import {FaBed,FaBath} from 'react-icons/fa'
+import {TbSquaresFilled} from 'react-icons/tb'
+import {AiOutlineHeart,AiFillHeart} from 'react-icons/ai'
+import {BsStars} from 'react-icons/bs'
 
-export default function Card() {
-  const imageUrl = "assets/house.jpg"
+export default function Card({obj}) {
+  const imageUrl = 'assets/house' + (obj.id % 8 + 1) + '.jpg';
+
+  const [like, setLike] = React.useState(false);
+
+  const toggle=() => { 
+    setLike(old => !old);
+  }
+
   return (
-    <div className="cardContainer">
-      <div className="houseImage" style={{backgroundImage:`url("${imageUrl}")`}}>
-          </div>
-          <div className="info">
-              <div className="rentalInfo">
-                  <div className="price">
-                      
-                  </div>
-                  <div className="name">
-                      
-                  </div>
-                  <div className="address">
-                      
-                  </div>
+      <div className="cardContainer">
+      <div className="houseImage" style={{ backgroundImage: `url("${imageUrl}")` }}>
+      {obj.popularity&&<div className="pop"><BsStars/> POPULAR</div>}
+      </div>
+        <div className="info">
+          <div className="rentalInfo">
+            <div className="price">
+              <div className="amt">
+                <span className="pricing">{'$'+obj.price}</span> / month
               </div>
-              <div className="houseInfo">
-                  <div className="rooms">
-                      
-                  </div>
-                  <div className="area">
-                      
-                  </div>
+              <div className="heart">
+                {!like?<AiOutlineHeart className='heartSym' onClick={()=>{toggle()}}/>:<AiFillHeart className='heartSym' onClick={()=>{toggle()}}/>}
               </div>
+            </div>
+            <div className="name">
+              {obj.region}
+            </div>
+            <div className="address">
+              {obj.address}
+            </div>     
           </div>
-    </div>
+          <div className="houseInfo">
+            <div className="rooms">
+              <FaBed className ='infoIcons'/>
+              {obj.rooms} Beds
+            </div>
+            <div className="bath">
+              <FaBath className ='infoIcons'/>
+              {obj.bath} Bath
+            </div>
+            <div className="area">
+              <TbSquaresFilled className ='infoIcons'/>
+              {obj.area} m<sup>2</sup>&nbsp;Area
+            </div>
+          </div>
+        </div>
+      </div>
   )
 }
